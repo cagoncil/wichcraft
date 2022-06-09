6566dd9b426dd8b018ffb354521fd4b1bcce38e2
cagoncil <crystallophobia@gmail.com>
2022-06-08 20:50:44 -0700

    initial commit

---
+++ src/LandingPage.js
@@ -0,0 +1,13 @@
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+    	<div className="LandingPage-text">
+    	🍔 Hello from LandingPage.js 🍔
+    	</div>
+    </div>
+  );
+}
+
+export default LandingPage;
