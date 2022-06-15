d790a12268e44d9bcb06ec1e3048456e603a455c
cagoncil <crystallophobia@gmail.com>
2022-06-15 13:32:13 -0700

    reorganize project architecture

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import LandingPage from './LandingPage';
-import Navbar from './Navbar';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import LandingPage from './components/LandingPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
